6ec2633f256f2aff26981584ebc731b3f6e009bd
IEDylan <dilanayalamurillo@gmail.com>
2025-02-03 19:08:07 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+var amigosSorteo = [];
+
+function agregarAmigo(){
+    let nombreParticipante = document.getElementById("amigo").value;
+    if(nombreParticipante == "" ){
+        alert("Estimado usuario, por favor ingrese un nombre!");
+    }else{
+        amigosSorteo.push(nombreParticipante)
+        document.getElementById("amigo").value="";
+        actualizarListAmigos()
+    }
+}
+
+function actualizarListAmigos(){
+    const listaAmimgos = document.getElementById("listaAmigos")
+    listaAmimgos.innerHTML="";
+
+    amigosSorteo.forEach(amigosregistrados=>{
+        const items = document.createElement("li");
+        items.textContent = amigosregistrados;
+        listaAmimgos.appendChild(items);
+    });
+}
+
+function sortearAmigo(){
+    if(amigosSorteo == ""){
+        alert("Aun no hay amigos asignados para hacer el sorteo")
+    }else{
+        let indiceGanador = Math.floor(Math.random()*amigosSorteo.length)
+
+        let amigoVictorioso = amigosSorteo[indiceGanador]
+
+        let resultado = document.getElementById("resultado");
+
+        resultado.innerHTML=amigoVictorioso;
+    }
+}
